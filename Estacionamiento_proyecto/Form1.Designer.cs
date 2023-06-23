3d46baa3845ad2bd18d219cee0960e1af1b2d23e
annymane <stpmansill@upana.edu.gt>
2023-06-23 14:33:36 -0600

    Avance con los carros en movimiento

--- Estacionamiento_proyecto/Form1.Designer.cs
+++ Estacionamiento_proyecto/Form1.Designer.cs
@@ -30,52 +30,86 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pbxCarrito = new System.Windows.Forms.PictureBox();
+            this.pbxCarrito1 = new System.Windows.Forms.PictureBox();
             this.pbxCarrito2 = new System.Windows.Forms.PictureBox();
+            this.pbxCarrito3 = new System.Windows.Forms.PictureBox();
+            this.pbxCarrito4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito4)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1238, 757);
+            this.pictureBox1.Size = new System.Drawing.Size(1347, 748);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // pbxCarrito
+            // pbxCarrito1
             // 
-            this.pbxCarrito.Location = new System.Drawing.Point(12, 248);
-            this.pbxCarrito.Name = "pbxCarrito";
-            this.pbxCarrito.Size = new System.Drawing.Size(100, 69);
-            this.pbxCarrito.TabIndex = 1;
-            this.pbxCarrito.TabStop = false;
+            this.pbxCarrito1.Image = ((System.Drawing.Image)(resources.GetObject("pbxCarrito1.Image")));
+            this.pbxCarrito1.Location = new System.Drawing.Point(23, 253);
+            this.pbxCarrito1.Name = "pbxCarrito1";
+            this.pbxCarrito1.Size = new System.Drawing.Size(84, 58);
+            this.pbxCarrito1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCarrito1.TabIndex = 1;
+            this.pbxCarrito1.TabStop = false;
             // 
             // pbxCarrito2
             // 
-            this.pbxCarrito2.Location = new System.Drawing.Point(13, 431);
+            this.pbxCarrito2.Image = ((System.Drawing.Image)(resources.GetObject("pbxCarrito2.Image")));
+            this.pbxCarrito2.Location = new System.Drawing.Point(23, 434);
             this.pbxCarrito2.Name = "pbxCarrito2";
-            this.pbxCarrito2.Size = new System.Drawing.Size(100, 66);
+            this.pbxCarrito2.Size = new System.Drawing.Size(84, 58);
+            this.pbxCarrito2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbxCarrito2.TabIndex = 2;
             this.pbxCarrito2.TabStop = false;
+            // 
+            // pbxCarrito3
+            // 
+            this.pbxCarrito3.Image = ((System.Drawing.Image)(resources.GetObject("pbxCarrito3.Image")));
+            this.pbxCarrito3.Location = new System.Drawing.Point(-70, 253);
+            this.pbxCarrito3.Name = "pbxCarrito3";
+            this.pbxCarrito3.Size = new System.Drawing.Size(87, 58);
+            this.pbxCarrito3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCarrito3.TabIndex = 3;
+            this.pbxCarrito3.TabStop = false;
+            this.pbxCarrito3.Visible = false;
+            // 
+            // pbxCarrito4
+            // 
+            this.pbxCarrito4.Image = ((System.Drawing.Image)(resources.GetObject("pbxCarrito4.Image")));
+            this.pbxCarrito4.Location = new System.Drawing.Point(-70, 434);
+            this.pbxCarrito4.Name = "pbxCarrito4";
+            this.pbxCarrito4.Size = new System.Drawing.Size(87, 58);
+            this.pbxCarrito4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCarrito4.TabIndex = 4;
+            this.pbxCarrito4.TabStop = false;
+            this.pbxCarrito4.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1238, 747);
+            this.ClientSize = new System.Drawing.Size(1342, 747);
+            this.Controls.Add(this.pbxCarrito4);
+            this.Controls.Add(this.pbxCarrito3);
             this.Controls.Add(this.pbxCarrito2);
-            this.Controls.Add(this.pbxCarrito);
+            this.Controls.Add(this.pbxCarrito1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCarrito4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,8 +117,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pbxCarrito;
+        private System.Windows.Forms.PictureBox pbxCarrito1;
         private System.Windows.Forms.PictureBox pbxCarrito2;
+        private System.Windows.Forms.PictureBox pbxCarrito3;
+        private System.Windows.Forms.PictureBox pbxCarrito4;
     }
 }
 
--- Estacionamiento_proyecto/Form1.cs
+++ Estacionamiento_proyecto/Form1.cs
@@ -12,16 +12,31 @@ namespace Estacionamiento_proyecto
 {
     public partial class Form1 : Form
     {
+        private List<PictureBox> carrosEnMovimiento = new List<PictureBox>();
+
         //variables para el pictureBox
         private int direction = 1; // Dirección inicial del movimiento
         private int speed = 2; // Velocidad de movimiento
         private int limit = 140; // Límite del movimiento
         private bool stopMovement = false; // Variable para controlar la detención
+        private Queue<PictureBox> carrosEspera = new Queue<PictureBox>();
+
 
         public Form1()
         {
             InitializeComponent(); 
             Application.Idle += Movimiento_pic;
+
+            // Agregar los dos primeros carros a la lista y hacerlos visibles
+            carrosEnMovimiento.Add(pbxCarrito1);
+            carrosEnMovimiento.Add(pbxCarrito2);
+
+
+            pbxCarrito1.Visible = true;
+            pbxCarrito2.Visible = true;
+            carrosEspera.Enqueue(pbxCarrito3);
+            carrosEspera.Enqueue(pbxCarrito4);
+
         }
 
         private async void Movimiento_pic(object sender, EventArgs e)
@@ -29,20 +44,20 @@ namespace Estacionamiento_proyecto
             if (!stopMovement)
             {
                 // Mover el PictureBox horizontalmente
-                pbxCarrito.Left += direction * speed;
+                pbxCarrito1.Left += direction * speed;
                 pbxCarrito2.Left += direction * speed;
 
-                // Comprobar límites
-                if (pbxCarrito.Left <= 0 || pbxCarrito.Left + pbxCarrito.Width >= limit || pbxCarrito2.Left <= 0 || pbxCarrito2.Left + pbxCarrito2.Width >= limit)
+                // Comprobar límites1
+                if (pbxCarrito1.Left <= 0 || pbxCarrito1.Left + pbxCarrito1.Width >= limit || pbxCarrito2.Left <= 0 || pbxCarrito2.Left + pbxCarrito2.Width >= limit)
                 {
                     // Cambiar la dirección cuando se alcanza un límite
                     direction *= -1;
 
                     // Detener el movimiento si se alcanza el límite deseado
-                    if (pbxCarrito.Left <= 0)
-                        pbxCarrito.Left = 0;
-                    else if (pbxCarrito.Left + pbxCarrito.Width >= limit)
-                        pbxCarrito.Left = limit - pbxCarrito.Width;
+                    if (pbxCarrito1.Left <= 0)
+                        pbxCarrito1.Left = 0;
+                    else if (pbxCarrito1.Left + pbxCarrito1.Width >= limit)
+                        pbxCarrito1.Left = limit - pbxCarrito1.Width;
 
                     if (pbxCarrito2.Left <= 0)
                         pbxCarrito2.Left = 0;
@@ -51,38 +66,60 @@ namespace Estacionamiento_proyecto
 
                     stopMovement = true;
 
-
                     Random random = new Random();
-                    int selectedPictureBox = random.Next(1, 3); // Generar un número aleatorio entre 1 y 2
 
-                    if (selectedPictureBox == 1)
+                    // Verificar si se oculta pbxCarrito1 o pbxCarrito2 primero
+                    if (random.Next(2) == 0)
                     {
-                        // Generar un número aleatorio entre 3000 y 5000 para pbxCarrito
+                        // Generar un número aleatorio entre 3000 y 5000 para el tiempo de espera de pbxCarrito1
                         int delayMilliseconds1 = random.Next(3000, 5001);
                         await Task.Delay(delayMilliseconds1);
 
-                        pbxCarrito.Visible = false; // Ocultar el pbxCarrito
+                        pbxCarrito1.Visible = false; // Ocultar pbxCarrito1
 
-                        // Generar un número aleatorio diferente entre 3000 y 5000 para pbxCarrito2
+                        // Generar un número aleatorio diferente entre 3000 y 5000 para el tiempo de espera de pbxCarrito2
                         int delayMilliseconds2 = random.Next(3000, 5001);
                         await Task.Delay(delayMilliseconds2);
 
-                        pbxCarrito2.Visible = false; // Ocultar el pbxCarrito2
+                        pbxCarrito2.Visible = false; // Ocultar pbxCarrito2
                     }
                     else
                     {
+                        // Generar un número aleatorio entre 3000 y 5000 para el tiempo de espera de pbxCarrito2
                         int delayMilliseconds2 = random.Next(3000, 5001);
                         await Task.Delay(delayMilliseconds2);
 
-                        pbxCarrito2.Visible = false; // Ocultar el pbxCarrito2
+                        pbxCarrito2.Visible = false; // Ocultar pbxCarrito2
 
+                        // Generar un número aleatorio diferente entre 3000 y 5000 para el tiempo de espera de pbxCarrito1
                         int delayMilliseconds1 = random.Next(3000, 5001);
                         await Task.Delay(delayMilliseconds1);
 
-                        pbxCarrito.Visible = false; // Ocultar el pbxCarrito
+                        pbxCarrito1.Visible = false; // Ocultar pbxCarrito1
                     }
+
+                    // Remover los carros de la lista y detener su movimiento
+                    carrosEnMovimiento.Remove(pbxCarrito1);
+                    carrosEnMovimiento.Remove(pbxCarrito2);
+                    stopMovement = true;
                 }
+
             }
+
+            // Verificar si no hay carros en movimiento y aún hay carros en espera
+            if (carrosEnMovimiento.Count == 0 && carrosEspera.Count > 0)
+            {
+                // Seleccionar el primer carro en espera y hacerlo visible
+                PictureBox nuevoCarro = carrosEspera.Dequeue();
+                nuevoCarro.Visible = true;
+
+                // Agregar el carro a la lista de carros en movimiento
+                carrosEnMovimiento.Add(nuevoCarro);
+
+                // Reiniciar el movimiento
+                stopMovement = false;
+            }
+
         }
     }
 }
